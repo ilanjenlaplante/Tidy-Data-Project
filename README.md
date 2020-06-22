c9b62e2457583026ab087e559d8a2583cc986700
Ilan Jen-La Plante <ilanjenlaplante@gmail.com>
2020-06-21 23:00:13 -0700

    Final project including updates to the README.md, a codebook, a script to form the final tidy dataset, and the tidy dataset itself

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Tidy-Data-Project
 Getting and Cleaning Data Week 4 Programming Assignment
+
+Data is downloaded and unzipped locally.
+
+Relevant data files within train and test folders are read in and combined
+into a single dataset. Test data is first combined with the Subject (test_subject),
+Activity (test_Y), and then all accelerometer data (test_X). The same is done for
+the training data set and then the two are combined using rbind to form a single
+dataset with dimensions of 10299 x 563.
+
+Descriptive variable names are added as column headers using the "features.txt"
+file and manually naming "Subject" and "Activity".
+
+From the original accelerometer data, only columns that refer to either mean()
+or std() are included (this does not include meanFreq).
+
+The string labels for each activity type are added as an additional column.
+
+The data is then grouped by subject and activity type and the mean of each
+measurement column is taken. The labels are activity are added back in to the
+file and then it is re-ordered by subject and activity. Since there are 6 possible 
+activities and 30 subjects, this produces a dataset with 180 rows and 87 columns. 
+
+Finally, the dataset is written out to a .txt. file.
+
+
---
+++ run_analysis.R
@@ -0,0 +1,50 @@
+## This is a script to merge, and clean data from Samsung Galaxy S smartphone accelerometers
+
+##Download and unzip the relevant data
+data_url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(data_url, destfile = "./temp.zip")
+unzip("./temp.zip")
+
+train_subject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+train_X <- read.table("./UCI HAR Dataset/train/X_train.txt")
+train_Y <- read.table("./UCI HAR Dataset/train/y_train.txt")
+
+test_subject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+test_X <- read.table("./UCI HAR Dataset/test/X_test.txt")
+test_Y <- read.table("./UCI HAR Dataset/test/y_test.txt")
+
+activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
+
+##Merge train and test data into a single dataset
+test_data <- cbind(test_subject, test_Y, test_X)
+train_data <- cbind(train_subject, train_Y, train_X)
+all_data <- rbind(test_data, train_data)
+
+#Change header names into descriptive variable names
+new_names <- c("Subject", "Activity", as.character(features$V2))
+names(all_data) <- new_names
+
+##Select only relevant data columns
+library(dplyr)
+select_data <- select(all_data, matches("Subject|Activity|mean()|std()"))
+final_data <- select_data[,1:86]
+
+##Use activity labels to change from numbered activity to descriptive activities
+new_activity_headers <- c("Activity", "Activity Label")
+names(activity_labels) <- new_activity_headers
+
+labeled_data <- merge(activity_labels, final_data, by="Activity")
+
+##Group and summarize by mean
+tidy_data_set <- labeled_data %>%
+        group_by(Subject, Activity) %>%
+        summarize_if(is.numeric, mean)
+
+##Reorder final dataset by Subject, then by Activity
+tidy_data_set <- merge(activity_labels, tidy_data_set, by="Activity")
+tidy_data_set <- arrange(tidy_data_set, Subject, Activity)
+
+##Writes the output tidy dataset to a .txt file
+write.table(tidy_data_set, file = "./final-tidy-data.txt", append = FALSE, sep = " ", dec = ".",
+            row.names = TRUE, col.names = TRUE)
